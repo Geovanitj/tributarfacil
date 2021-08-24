563c3688a251b2a067750df3778b5e1cb24b0757
geovani <geovany.tj@gmail.com>
2021-08-24 08:53:09 -0300

    Pagina CFOP

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
 $(function() {
     $(window).on("scroll", function() {
-        if ($(window).scrollTop() > 100) {
-            $(".header").addClass("heraderMudaCor");
+        if ($(window).scrollTop() > 50) {
+            $(".header").addClass("hearderMudaCor");
         } else {
-            $(".header").removeClass("heraderMudaCor")
+            $(".header").removeClass("hearderMudaCor")
         }
     })
 })
